2fdd6b543517d2dd66566abf5c795930d9aee8b5
harshg28101999 <hmg5265@psu.edu>
2021-04-08 03:11:12 +0530

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, Button} from 'react-native';
+
+// import {createStackNavigator} from 'react-navigation';
+// import Steps from './Components/Steps'
+
+// const Rootstack = createStackNavigator({
+//   Steps: Steps
+// })
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      
+      <Image style={styles.image} source={require('./assets/solve_logo.png')} />
+      <Text style={styles.title}>Solve</Text>
+      <Text>On-Demand Virtual Shopping and Delivery</Text>
       <StatusBar style="auto" />
-    </View>
+
+      <Button title="Continue >" />
+      
+      <Button title="Login >" />
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+
+  image: {
+    width: 150,
+    height: 150
   },
 });
---
+++ Components/Sign_up.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Sign_up
+ extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
